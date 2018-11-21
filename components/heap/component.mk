c89cb0ff82145cfbcceb4e2a5dad140cf336b598
yaozhenfa <976691141@qq.com>
2018-11-21 19:31:14 +0800

    修复问题

--- components/heap/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Component Makefile
-#
-
-COMPONENT_ADD_INCLUDEDIRS := include port/esp8266/include
-
-COMPONENT_SRCDIRS := src port/esp8266
--- components/wpa_supplicant/component.mk
+++
@@ -1,4 +0,0 @@
-COMPONENT_ADD_INCLUDEDIRS := include port/include
-COMPONENT_SRCDIRS := src/crypto port
-
-CFLAGS += -DEMBEDDED_SUPP -D__ets__ -DESPRESSIF_USE
